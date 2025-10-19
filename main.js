f9ae8ff72ff192a5a473935c524772b0534de21d
Maleesha Sanjana <maleeshasanjanadilshan@gmail.com>
2025-10-19 10:30:36 +0530

    Structure Of CodeSensei

---
+++ main.js
@@ -0,0 +1,51 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Keep a global reference of the window object
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false
+    },
+    titleBarStyle: 'hiddenInset',
+    show: false
+  });
+
+  // Load the HTML file
+  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+  // Show window when ready to prevent visual flash
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  // Emitted when the window is closed
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished initialization
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
